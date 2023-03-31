a344dbd75ce5c4792b00429de1513d6609e63344
Mateus de Oliveira Luciano <71831859+mateusluciano@users.noreply.github.com>
2023-03-31 15:49:12 -0300

    tipos primitivos

--- ex003/index.php
+++ ex003/index.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tipos primitivos em PHP</title>
 </head>
 <body>
-    
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = Hexadecimal  0b = Binário  0 = Octal
+
+        /* $num = 0x2B;
+        echo "O valor da variável é $num!"; */
+
+        $v = 300;
+        var_dump($v);
+    ?>
 </body>
 </html>
