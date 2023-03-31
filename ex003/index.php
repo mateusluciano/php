cf44c7e83a87f7b93c7913bed8590777b8c4269a
Mateus de Oliveira Luciano <71831859+mateusluciano@users.noreply.github.com>
2023-03-31 13:58:46 -0300

    att

--- ex003/index.php
+++ ex003/index.php
@@ -4,14 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dia!</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Exemplo de PHP</h1>
-    <?
-        date_default_timezone_set("America/Sao_Paulo");
-        echo "Hoje é dia " . date("d/m/Y");
-        echo "<br>E a hora atual é " . date("G:i:s T")
-    ?>
+    
 </body>
 </html>
